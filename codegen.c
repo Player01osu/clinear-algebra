aa5fd1dd04ccc0f0b5163649fdde7fec59f0cb07
player01osu <player01osu@gmail.com>
2024-08-06 13:50:43 -0400

    Static inline the functions

--- codegen.c
+++ codegen.c
@@ -1,11 +1,15 @@
+#define _XOPEN_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 void write_header(FILE *f)
 {
-	const char HEADER[] = "#ifndef VEC_TYPE\n"
+	const char HEADER[] = "/* This file was auto-generated with `https://github.com/Player01osu/clinear-algebra` */\n"
+		"#ifndef VEC_TYPE\n"
 		"#error Define a type with \"VEC_TYPE\"\n"
 		"#else\n"
 		"#define T VEC_TYPE\n"
@@ -35,7 +39,7 @@ void write_vec_struct_def(FILE *f, size_t dimension)
 
 void write_vec_add_func(FILE *f, size_t dimension)
 {
-	fprintf(f, "Vec%ld vec%ld_add(Vec%ld a_1, Vec%ld a_2)\n"
+	fprintf(f, "static inline Vec%ld vec%ld_add(Vec%ld a_1, Vec%ld a_2)\n"
 		"{\n"
 		"\treturn (Vec%ld) {\n",
 		dimension, dimension, dimension,
@@ -50,7 +54,7 @@ void write_vec_add_func(FILE *f, size_t dimension)
 
 void write_vec_scale_func(FILE *f, size_t dimension)
 {
-	fprintf(f, "Vec%ld vec%ld_scale(Vec%ld a_1, T scalar)\n"
+	fprintf(f, "static inline Vec%ld vec%ld_scale(Vec%ld a_1, T scalar)\n"
 		"{\n"
 		"\treturn (Vec%ld) {\n",
 		dimension, dimension, dimension, dimension);
@@ -64,7 +68,7 @@ void write_vec_scale_func(FILE *f, size_t dimension)
 
 void write_vec_dot_func(FILE *f, size_t dimension)
 {
-	fprintf(f, "T vec%ld_dot(Vec%ld a_1, Vec%ld a_2)\n{\n",
+	fprintf(f, "static inline T vec%ld_dot(Vec%ld a_1, Vec%ld a_2)\n{\n",
 		dimension, dimension, dimension);
 
 	for (size_t i = 0; i < dimension; ++i) {
@@ -89,7 +93,7 @@ void write_mat_struct_def(FILE *f, size_t rows, size_t cols)
 
 void write_mat_vec_mult_func(FILE *f, size_t rows, size_t cols)
 {
-	fprintf(f, "Vec%ld mat%ldx%ld_vec%ld_mult(Mat%ldx%ld m, Vec%ld a)\n"
+	fprintf(f, "static inline Vec%ld mat%ldx%ld_vec%ld_mult(Mat%ldx%ld m, Vec%ld a)\n"
 		"{\n",
 		rows, rows, cols, cols,
 		rows, cols, cols);
@@ -103,6 +107,10 @@ void write_mat_vec_mult_func(FILE *f, size_t rows, size_t cols)
 		fprintf(f, "}, a),\n");
 	}
 	fprintf(f, "\t};\n""}\n");
+}
+
+void write_mat_mat_mult_func(FILE *f, size_t rows, size_t cols)
+{
 }
 
 void write_vec_structs(FILE *f, size_t min_d, size_t max_d, bool write_mat)
@@ -134,12 +142,45 @@ void write_vec_structs(FILE *f, size_t min_d, size_t max_d, bool write_mat)
 	}
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
-	FILE *f = fopen("linear_algebra.h", "wb");
+	int l = -1;
+	int h = -1;
+	char *o = NULL;
+	char c;
+	while ((c = getopt(argc, argv, "l:h:o:")) != -1) {
+		switch (c) {
+		case 'l':
+			l = atoi(optarg);
+			break;
+		case 'h':
+			h = atoi(optarg);
+			break;
+		case 'o':
+			o = optarg;
+			break;
+		default:
+			fprintf(stderr, "Invalid argument\n");
+			exit(1);
+		}
+	}
+	if (l < 0) {
+		fprintf(stderr, "'l': lowerbound not given\n");
+		exit(1);
+	}
+	if (h < 0) {
+		fprintf(stderr, "'h': upperbound not given\n");
+		exit(1);
+	}
+	if (!o) {
+		fprintf(stderr, "'o': Output filename not given\n");
+		exit(1);
+	}
+
+	FILE *f = fopen(o, "wb");
 	write_header(f);
 
-	write_vec_structs(f, 2, 10, true);
+	write_vec_structs(f, l, h, true);
 
 	fclose(f);
 	return 0;
--- x
+++ x
@@ -11,5 +11,5 @@ ${CC} -o ${OUT} ${SRC} ${CFLAGS}
 
 ./${OUT}
 
-${CC} -o linear_algebra_test test.c ${CFLAGS}
-./linear_algebra_test
+#${CC} -o linear_algebra_test test.c ${CFLAGS} -fdata-sections -ffunction-sections -Wl,--gc-sections
+#./linear_algebra_test
